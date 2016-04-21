a2dbbfb37623d990ea231f8d9ba104d85ac9d792
z00842877 <0842877@studentmail.ul.ie>
2016-04-21 20:56:45 +0100

    First Intent

--- app/src/main/java/com/example/z00842877/ulbikespots/EntryScreen.java
+++ app/src/main/java/com/example/z00842877/ulbikespots/EntryScreen.java
@@ -1,16 +1,20 @@
 package com.example.z00842877.ulbikespots;
 
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -20,76 +24,109 @@ import java.util.HashSet;
 
 public class EntryScreen extends AppCompatActivity {
 
-
-    MapFragment mMapFragment;
-    ArrayList<Parking> parkingspots;
+    private MapFragment mMapFragment;
+    public HashSet<Parking> parkingspots;
+    private HashSet<Parking> tempParkingSpots;
+    public String userType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_screen);
-        createHashSet();
+        ParkingSet test = new ParkingSet();
+        parkingspots = test.parkingspots;
+        specifyHashSet();
 
         mMapFragment = MapFragment.newInstance();
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.map, mMapFragment);
         fragmentTransaction.commit();
-        /*
-        mMapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                googleMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(52.66136550517293, -8.624267575625026))
-                        .title("Limerick")
-                        .snippet("Come here for the craic!"));
-                googleMap.moveCamera(CameraUpdateFactory
-                        .newLatLngZoom(new LatLng(52.66136550517293, -8.624267575625026), 7));
-            }
-        });
-        */
 
         mMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                for( Parking x : parkingspots) {
+                for (Parking x : tempParkingSpots) {
                     googleMap.addMarker(new MarkerOptions()
                             .position(new LatLng(x.getLatitude(), x.getLongitude()))
                             .title(x.getLocation())
-                            .snippet("Suits "+ x.getUserType()+" Users"));
-                    googleMap.moveCamera(CameraUpdateFactory
-                            .newLatLngZoom(new LatLng(52.67514489282713, -8.571814621734575), 15));
+                            .snippet(x.toString())
+                            .icon(BitmapDescriptorFactory.defaultMarker(x.getColor())));
                 }
+                googleMap.moveCamera(CameraUpdateFactory
+                        .newLatLngZoom(new LatLng(52.67514489282713, -8.571814621734575), 15));
+            }
+        });
+
+        Button buttonNext = (Button)findViewById(R.id.but1);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), FeatureList.class);
+                intent.putExtra("userType", userType);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonBut2 = (Button)findViewById(R.id.but2);
+        buttonBut2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), FeatureList.class);
+                intent.putExtra("userType", userType);
+                startActivity(intent);
             }
         });
 
 
     }
 
-    public void createHashSet(){
-        parkingspots = new ArrayList<>();
-        parkingspots.add(new Parking("KBS Back","ALL",52.67246556,-8.577017782,false,false,true));
-        parkingspots.add(new Parking("KBS Front","ALL",52.67272578,-8.577339647,false,false,true));
-        parkingspots.add(new Parking("CSIS Carpark","ALL",52.67436516,-8.575156329,false,false,false));
-        parkingspots.add(new Parking("Foundation Carpark","STAFF",52.67457334,-8.57491493,true,true,true));
-        parkingspots.add(new Parking("ERG Carpark","STAFF",52.67542553,-8.573032019,true,true,true));
-        parkingspots.add(new Parking("Stokes Research Institute","ALL",52.67484005,-8.572710154,true,false,true));
-        parkingspots.add(new Parking("Library Entrance","ALL",52.67338284,-8.573176858,false,false,true));
-        parkingspots.add(new Parking("Sundial","ALL",52.6733308,-8.571438787,false,false,false));
-        parkingspots.add(new Parking("Visitor Carpark","VISITOR",52.67302178,-8.571642635,false,false,false));
-        parkingspots.add(new Parking("Student Union","ALL",52.67332755,-8.570135233,true,false,false));
-        parkingspots.add(new Parking("Analog Devices","ALL",52.67306732,-8.568869231,false,false,false));
-        parkingspots.add(new Parking("Arena Carpark","ALL",52.6728689,-8.56509268,true,false,true));
-        parkingspots.add(new Parking("Arena","ALL",52.67326249,-8.564985392,false,false,true));
-        parkingspots.add(new Parking("PESS","ALL",52.67442209,-8.568005559,false,false,true));
-        parkingspots.add(new Parking("Health Sciences","ALL",52.67766814,-8.569266198,false,false,true));
-        parkingspots.add(new Parking("Irish World Academy","ALL",52.67823081,-8.569700715,false,false,true));
-        parkingspots.add(new Parking("Graduate Medical School","ALL",52.67823406,-8.568080661,false,false,true));
-        parkingspots.add(new Parking("Pavillion","ALL",52.67872517,-8.569867012,false,false,true));
-        parkingspots.add(new Parking("Schuman","ALL",52.67356012,-8.577715156,false,false,true));
+    public void specifyHashSet(){
+        tempParkingSpots = new HashSet<>();
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            userType = extras.getString("userType");
+            for(Parking x : parkingspots){
+                if(x.getUserType().equalsIgnoreCase(userType) || x.getUserType().equalsIgnoreCase("ALL")){
+                    tempParkingSpots.add(x);
+                }
+            }
 
+            String buildingName = extras.getString("buildingName");
+            String featureType = extras.getString("featureType");
+            HashSet<Parking> tempRemove =new  HashSet<>();
+            if(featureType != null) {
+                if (featureType.equalsIgnoreCase("Shower")) {
+                    for (Parking x : tempParkingSpots) {
+                        if (!x.hasShower()) {
+                            tempRemove.add(x);
+                        }
+                    }
+                } else if (featureType.equalsIgnoreCase("Coverage")) {
+                    for (Parking x : tempParkingSpots) {
+                        if (!x.isCovered()) {
+                            tempRemove.add(x);
+                        }
+                    }
+                } else if (featureType.equalsIgnoreCase("Secure")) {
+                    for (Parking x : tempParkingSpots) {
+                        if (!x.isSecure()) {
+                            tempRemove.add(x);
+                        }
+                    }
+                }
+                tempParkingSpots.removeAll(tempRemove);
+            }  else if(buildingName != null) {
+                for (Parking x : tempParkingSpots) {
+                    if (!x.getBuildingName().equalsIgnoreCase(buildingName)) {
+                        tempRemove.add(x);
+                    }
+                }
+            }
 
+        }else{
+            tempParkingSpots = parkingspots;
+        }
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
