95193f68ceeeb07bd80848f24d02518f4d99d6c2
z00842877 <0842877@studentmail.ul.ie>
2016-04-21 20:59:50 +0100

    FMost of the stuff

---
+++ app/src/main/java/com/example/z00842877/ulbikespots/List.java
@@ -0,0 +1,77 @@
+package com.example.z00842877.ulbikespots;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class List extends ActionBarActivity {
+    ArrayList<String> note;
+    ArrayAdapter<String> noteAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        setup();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setup(){
+        note = new ArrayList<>();
+        //instantiate adapter
+        note.add("Visitor");
+        note.add("STAFF");
+        note.add("ALL");
+        noteAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, note);
+
+        //attach adapter to Listview
+        ListView noteListView = (ListView) findViewById(R.id.note_list_view);
+        noteListView.setAdapter(noteAdapter);
+
+        noteListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemData = note.get(position);
+                Toast.makeText(parent.getContext(), "Clicked " + itemData, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(view.getContext(), EntryScreen.class);
+                intent.putExtra("userType", note.get(position));
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/example/z00842877/ulbikespots/Parking.java
+++ app/src/main/java/com/example/z00842877/ulbikespots/Parking.java
@@ -1,5 +1,7 @@
 package com.example.z00842877.ulbikespots;
 
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
 import java.util.HashSet;
 
 public class Parking {
@@ -10,32 +12,7 @@ public class Parking {
     private boolean secure;
     private String user;
     private boolean shower;
-
-    public static void main(String[]args) {
-        HashSet<Parking> parkingspots = new HashSet<>();
-        parkingspots.add(new Parking("KBS Back", "ALL", 52.67246556, -8.577017782, false, false, true));
-        parkingspots.add(new Parking("KBS Front", "ALL", 52.67272578, -8.577339647, false, false, true));
-        parkingspots.add(new Parking("CSIS Carpark", "ALL", 52.67436516, -8.575156329, false, false, false));
-        parkingspots.add(new Parking("Foundation Carpark", "STAFF", 52.67457334, -8.57491493, true, true, true));
-        parkingspots.add(new Parking("ERG Carpark", "STAFF", 52.67542553, -8.573032019, true, true, true));
-        parkingspots.add(new Parking("Stokes Research Institute", "ALL", 52.67484005, -8.572710154, true, false, true));
-        parkingspots.add(new Parking("Library Entrance", "ALL", 52.67338284, -8.573176858, false, false, true));
-        parkingspots.add(new Parking("Sundial", "ALL", 52.6733308, -8.571438787, false, false, false));
-        parkingspots.add(new Parking("Visitor Carpark", "VISITOR", 52.67302178, -8.571642635, false, false, false));
-        parkingspots.add(new Parking("Student Union", "ALL", 52.67332755, -8.570135233, true, false, false));
-        parkingspots.add(new Parking("Analog Devices", "ALL", 52.67306732, -8.568869231, false, false, false));
-        parkingspots.add(new Parking("Arena Carpark", "ALL", 52.6728689, -8.56509268, true, false, true));
-        parkingspots.add(new Parking("Arena", "ALL", 52.67326249, -8.564985392, false, false, true));
-        parkingspots.add(new Parking("PESS", "ALL", 52.67442209, -8.568005559, false, false, true));
-        parkingspots.add(new Parking("Health Sciences", "ALL", 52.67766814, -8.569266198, false, false, true));
-        parkingspots.add(new Parking("Irish World Academy", "ALL", 52.67823081, -8.569700715, false, false, true));
-        parkingspots.add(new Parking("Graduate Medical School", "ALL", 52.67823406, -8.568080661, false, false, true));
-        parkingspots.add(new Parking("Pavillion", "ALL", 52.67872517, -8.569867012, false, false, true));
-        parkingspots.add(new Parking("Schuman", "ALL", 52.67356012, -8.577715156, false, false, true));
-        for (Parking x : parkingspots){
-            System.out.println(x.getLatitude());
-        }
-    }
+    private float color;
 
     public Parking(String loc,String User, double lat, double lon,
                     boolean coverage, boolean secure, boolean shower){
@@ -46,8 +23,18 @@ public class Parking {
         this.coverage = coverage;
         this.secure = secure;
         this.shower = shower;
+        if(User.equalsIgnoreCase("Visitor")){
+            this.color = BitmapDescriptorFactory.HUE_AZURE;
+        }else if (User.equalsIgnoreCase("ALL")) {
+            this.color = BitmapDescriptorFactory.HUE_GREEN;
+        }else{
+            this.color = BitmapDescriptorFactory.HUE_RED;
+        }
     }
     public String getLocation(){
+        return this.location;
+    }
+    public String getBuildingName(){
         return this.location;
     }
     public String getUserType(){
@@ -67,6 +54,22 @@ public class Parking {
     }
     public boolean hasShower(){
         return this.shower;
+    }
+    public float getColor(){
+        return this.color;
+    }
+    public String toString(){
+        String test="";
+        if(this.coverage){
+            test+="-Is Covered ";
+        }
+        if(this.secure){
+            test+="-Is Secure";
+        }
+        if(this.shower){
+            test+="-Has Shower";
+        }
+        return test;
     }
 
 }
